fca1d7777e8b5f90d950c41d91a5547a896c6a47
Noé Medina González <medinagonzaleznoe@gmail.com>
2024-05-06 11:07:04 +0200

    Funcionando menos seccion culturetas

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2')
 
 // Middleware para servir archivos estáticos
-app.use(express.static('public'));
+app.use(express.static('pages'));
+app.use(express.static('imagenes'));
 
 // Motor de plantillas
 app.set('view engine', 'ejs');
 
 // Rutas
 app.get('/', (req, res) => {
-    res.render('index');
+    res.sendFile(__dirname + '/pages/index.html');
 });
 
 // Más configuraciones y rutas...
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log(`Servidor corriendo`);
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mrOlympiaRanking',//Nombre de la base de datos
+});
+
+db.connect((err) => {
+    if (err){
+        console.log('Error al conectar a BD: ', err);
+        return;
+    }
+    console.log('Conexion realizada');
 });
